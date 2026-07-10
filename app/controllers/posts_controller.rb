52416b8dab2efa9797f03c0893d2a635783385e0
sweta <sweeta.gumanju@gmail.com>
2026-07-10 16:34:49 +0545

    add create action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,3 +6,18 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 end
+
+def create
+  @post = current_user.posts.create(post_params)
+  if @post.valid?
+    redirect_to_root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:photo, :description)
+end
